5ffe1a11fa1c65579078692aef0486646b49361d
Terrarianec <dimaryazantcev@gmail.com>
2024-05-01 18:24:10 +0300

    Улучшено отображение элементов страницы

--- GameStore.DB/Models/Extensions/GameExtension.cs
+++ GameStore.DB/Models/Extensions/GameExtension.cs
@@ -3,5 +3,6 @@
 	public partial class Game
 	{
 		public string StringifiedPrice => Price > 0 ? $"{Price:N2} руб." : "Бесплатно";
+		public string StringifiedRequiredSpace => RequiredFreeSpace > 1000 ? $"{RequiredFreeSpace / 1000D} GB" : $"{RequiredFreeSpace} MB";
 	}
 }
