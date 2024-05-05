be4df358c9ca24ae53615823583e7135b46b4f39
Terrarianec <dimaryazantcev@gmail.com>
2024-05-05 23:35:58 +0300

    Покупка игры реализована через процедуру

--- GameStore.Presentation/Pages/GamePage.xaml.cs
+++ GameStore.Presentation/Pages/GamePage.xaml.cs
@@ -1,165 +1,171 @@
 ﻿using GameStore.DB;
 using GameStore.DB.Models;
 using GameStore.Presentation.Windows;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace GameStore.Presentation.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для GamePage.xaml
-    /// </summary>
-    public partial class GamePage : UserControl
-    {
-        private readonly GameStoreContext _context = new();
-        private readonly Game _game;
-        private GameReview _review = new();
+	/// <summary>
+	/// Логика взаимодействия для GamePage.xaml
+	/// </summary>
+	public partial class GamePage : UserControl
+	{
+		private readonly GameStoreContext _context = new();
+		private readonly Game _game;
+		private GameReview _review = new();
 
-        public GamePage(Game game)
-        {
-            InitializeComponent();
+		public GamePage(Game game)
+		{
+			InitializeComponent();
 
-            _game = _context.Games
-                .AsNoTracking()
-                .Include(g => g.Team)
-                .Include(g => g.Tags)
-                .Include(g => g.Reviews)
-                .ThenInclude(r => r.User)
-                .Where(g => g.Id == game.Id)
-                .FirstOrDefault() ?? new Game();
+			_game = _context.Games
+				.AsNoTracking()
+				.Include(g => g.Team)
+				.Include(g => g.Tags)
+				.Include(g => g.Reviews)
+				.ThenInclude(r => r.User)
+				.Where(g => g.Id == game.Id)
+				.FirstOrDefault() ?? new Game();
 
-            DataContext = _game;
+			DataContext = _game;
 
-            if (_game?.Reviews.Count > 0)
-            {
-                var rate = _game.Reviews.Average(r => r.Rate);
-                rateFrogs.Width = rateFrogs.MaxWidth * rate / 5;
-                rateValue.Text = $"{rate:N2}";
-            }
+			if (_game?.Reviews.Count > 0)
+			{
+				var rate = _game.Reviews.Average(r => r.Rate);
+				rateFrogs.Width = rateFrogs.MaxWidth * rate / 5;
+				rateValue.Text = $"{rate:N2}";
+			}
 
-            reviewsListView.ItemsSource = _game!.Reviews.Where(r => r.UserId != MainWindow.User?.Id).ToList();
+			reviewsListView.ItemsSource = _game!.Reviews.Where(r => r.UserId != MainWindow.User?.Id).ToList();
 
-            if (MainWindow.User != null)
-            {
-                CheckPurchasedGame(MainWindow.User, _game!);
+			if (MainWindow.User != null)
+			{
+				CheckPurchasedGame(MainWindow.User, _game!);
 
-                _review = _game.Reviews
-                    .Where(r => r.UserId == MainWindow.User.Id)
-                    .FirstOrDefault()
-                    ?? new GameReview { GameId = _game.Id, User = MainWindow.User, Rate = 1, PublishDate = DateOnly.FromDateTime(DateTime.Now) };
+				_review = _game.Reviews
+					.Where(r => r.UserId == MainWindow.User.Id)
+					.FirstOrDefault()
+					?? new GameReview { GameId = _game.Id, User = MainWindow.User, Rate = 1, PublishDate = DateOnly.FromDateTime(DateTime.Now) };
 
-                reviewRate.Width = _review.Rate / 5D * reviewRate.MaxWidth;
+				reviewRate.Width = _review.Rate / 5D * reviewRate.MaxWidth;
 
-                myReview.DataContext = _review;
-            }
-        }
+				myReview.DataContext = _review;
+			}
+		}
 
-        private void OnTeamClick(object sender, RoutedEventArgs e)
-        {
-            var team = (Team)((ContentControl)sender).Tag;
+		private void OnTeamClick(object sender, RoutedEventArgs e)
+		{
+			var team = (Team)((ContentControl)sender).Tag;
 
-            MainWindow.SetActivePage(new TeamPage(team));
-        }
+			MainWindow.SetActivePage(new TeamPage(team));
+		}
 
-        private async void OnPurcaseButtonClickAsync(object sender, RoutedEventArgs e)
-        {
-            if (MainWindow.User == null)
-            {
-                MessageBox.Show("Самое время зарегистрироваться!");
-                return;
-            }
+		private void OnPurcaseButtonClick(object sender, RoutedEventArgs e)
+		{
+			if (MainWindow.User == null)
+			{
+				MessageBox.Show("Самое время зарегистрироваться!");
+				return;
+			}
 
-            if (MainWindow.User?.Balance < _game.Price)
-            {
-                MessageBox.Show("Недостаточно средств");
-                return;
-            }
+			if (MainWindow.User?.Balance < _game.Price)
+			{
+				MessageBox.Show("Недостаточно средств");
+				return;
+			}
 
-            var user = await _context.Users.Include(u => u.Games).FirstAsync(u => u.Id == MainWindow.User!.Id);
-            var game = await _context.Games.FirstAsync(g => g.Id == _game.Id);
+			SqlParameter statusCode = new()
+			{
+				ParameterName = "@StatusCode",
+				SqlDbType = System.Data.SqlDbType.Int,
+				Direction = System.Data.ParameterDirection.Output
+			};
 
-            user.Games.Add(game);
+			_context.Database.ExecuteSqlRaw("dbo.PurchaseGame @p0, @p1, @StatusCode OUT", _game.Id, MainWindow.User!.Id, statusCode);
 
-            await _context.SaveChangesAsync();
+			if ((int)statusCode.Value != 0)
+				MessageBox.Show("Не удалось приобрести игру");
+			else
+				MessageBox.Show("Приобретено");
 
-            MessageBox.Show("Приобретено :0");
+			if (MainWindow.User != null)
+				CheckPurchasedGame(MainWindow.User, _game);
+		}
 
-            if (MainWindow.User != null)
-                CheckPurchasedGame(MainWindow.User, _game);
-        }
+		private async void CheckPurchasedGame(User user, Game game)
+		{
+			var purchasedGames = await _context.Games
+				.Include(g => g.Users)
+				.Where(g => g.Users.Any(u => u.Id == user.Id))
+				.Select(g => g.Id)
+				.ToListAsync();
 
-        private async void CheckPurchasedGame(User user, Game game)
-        {
-            var purchasedGames = await _context.Games
-                .Include(g => g.Users)
-                .Where(g => g.Users.Any(u => u.Id == user.Id))
-                .Select(g => g.Id)
-                .ToListAsync();
+			if (purchasedGames.Any(id => id == game.Id))
+			{
+				purchaseButton.Visibility = Visibility.Hidden;
+				playButton.Visibility = Visibility.Visible;
+			}
+		}
 
-            if (purchasedGames.Any(id => id == game.Id))
-            {
-                purchaseButton.Visibility = Visibility.Hidden;
-                playButton.Visibility = Visibility.Visible;
-            }
-        }
+		private void OnPlayButtonClick(object sender, RoutedEventArgs e)
+		{
+			new GameWindow() { Owner = MainWindow.Instance }
+				.ShowDialog();
+		}
 
-        private void OnPlayButtonClick(object sender, RoutedEventArgs e)
-        {
-            new GameWindow() { Owner = MainWindow.Instance }
-                .ShowDialog();
-        }
+		private void OnReviewContentChanged(object sender, TextChangedEventArgs e)
+		{
+			var reviewContent = (TextBox)sender;
+			charactersLeftIndicator.Text = $"{reviewContent.MaxLength - reviewContent.Text.Length}";
+		}
 
-        private void OnReviewContentChanged(object sender, TextChangedEventArgs e)
-        {
-            var reviewContent = (TextBox)sender;
-            charactersLeftIndicator.Text = $"{reviewContent.MaxLength - reviewContent.Text.Length}";
-        }
+		private void OnRateChanged(object sender, RoutedEventArgs e)
+		{
+			var selectedFrog = (Button)sender;
+			var rate = ((StackPanel)selectedFrog.Parent).Children.IndexOf(selectedFrog) + 1;
 
-        private void OnRateChanged(object sender, RoutedEventArgs e)
-        {
-            var selectedFrog = (Button)sender;
-            var rate = ((StackPanel)selectedFrog.Parent).Children.IndexOf(selectedFrog) + 1;
+			if (_review is GameReview myReview)
+				myReview.Rate = rate;
 
-            if (_review is GameReview myReview)
-                myReview.Rate = rate;
+			reviewRate.Width = rate / 5D * reviewRate.MaxWidth;
+		}
 
-            reviewRate.Width = rate / 5D * reviewRate.MaxWidth;
-        }
+		private void OnPublishClick(object sender, RoutedEventArgs e)
+		{
+			if (_review.UserId != 0)
+			{
+				_context.GameReviews
+					.Where(r => r.UserId == MainWindow.User!.Id && r.GameId == _game.Id)
+					.ExecuteUpdate(r => r
+						.SetProperty(r => r.Rate, _review.Rate)
+						.SetProperty(r => r.Content, _review.Content)
+						.SetProperty(r => r.PublishDate, DateOnly.FromDateTime(DateTime.Now))
+				);
 
-        private void OnPublishClick(object sender, RoutedEventArgs e)
-        {
-            if (_review.UserId != 0)
-            {
-                _context.GameReviews
-                    .Where(r => r.UserId == MainWindow.User!.Id && r.GameId == _game.Id)
-                    .ExecuteUpdate(r => r
-                        .SetProperty(r => r.Rate, _review.Rate)
-                        .SetProperty(r => r.Content, _review.Content)
-                        .SetProperty(r => r.PublishDate, DateOnly.FromDateTime(DateTime.Now))
-                );
+				return;
+			}
 
-                return;
-            }
+			_review.UserId = _review.User.Id;
+			_review.User = null;
 
-            _review.UserId = _review.User.Id;
-            _review.User = null;
+			_context.GameReviews.Add(_review);
 
-            _context.GameReviews.Add(_review);
+			_context.SaveChanges();
+		}
 
-            _context.SaveChanges();
-        }
+		private void OnDeleteReviewClick(object sender, RoutedEventArgs e)
+		{
+			if (_review.UserId > 0)
+			{
+				_context.GameReviews
+					.Where(r => r.UserId == MainWindow.User!.Id && r.GameId == _game.Id)
+					.ExecuteDelete();
+			}
 
-        private void OnDeleteReviewClick(object sender, RoutedEventArgs e)
-        {
-            if (_review.UserId > 0)
-            {
-                _context.GameReviews
-                    .Where(r => r.UserId == MainWindow.User!.Id && r.GameId == _game.Id)
-                    .ExecuteDelete();
-            }
-
-            _review = new GameReview { GameId = _game.Id, User = MainWindow.User!, Rate = 1, PublishDate = DateOnly.FromDateTime(DateTime.Now) };
-        }
-    }
+			_review = new GameReview { GameId = _game.Id, User = MainWindow.User!, Rate = 1, PublishDate = DateOnly.FromDateTime(DateTime.Now) };
+		}
+	}
 }
