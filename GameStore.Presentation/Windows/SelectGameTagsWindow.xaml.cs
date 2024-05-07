f348740ea6ba62037857326acd66ed0aed7731a3
Terrarianec <dimaryazantcev@gmail.com>
2024-05-07 14:24:38 +0300

    Доступен поиск тегов

--- GameStore.Presentation/Windows/SelectGameTagsWindow.xaml.cs
+++ GameStore.Presentation/Windows/SelectGameTagsWindow.xaml.cs
@@ -16,21 +16,28 @@ namespace GameStore.Presentation.Windows
 		{
 			InitializeComponent();
 
+			_game = game;
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+
 			var allTags = _context.Tags.ToList();
 			tags.ItemsSource = allTags;
 
-			_game = game;
-
 			tags.SelectionChanged -= OnSelectionChanged;
 
-			foreach (var tag in game.Tags)
+			foreach (var tag in _game.Tags)
 			{
-				var selected = allTags.First(t => t.Id == tag.Id);
+				var selected = allTags.FirstOrDefault(t => t.Id == tag.Id);
 
-				tags.SelectedItems.Add(selected);
+				if (selected is not null)
+					tags.SelectedItems.Add(selected);
 			}
 
 			tags.SelectionChanged += OnSelectionChanged;
+
+			searchField.Focus();
 		}
 
 		private void OnSubmit(object sender, RoutedEventArgs e)
@@ -45,6 +52,29 @@ namespace GameStore.Presentation.Windows
 
 			foreach (Tag removed in e.RemovedItems)
 				_game.Tags.Remove(removed);
+		}
+
+		private void OnSearchValueChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+			tags.SelectionChanged -= OnSelectionChanged;
+
+			var selectedTags = (searchField.Text.Length > 0
+				? _context.Tags.Where(t => t.Name.ToLower().Contains(searchField.Text.ToLower()))
+				: _context.Tags)
+				.ToList();
+
+			tags.ItemsSource = selectedTags;
+
+
+			foreach (var tag in _game.Tags)
+			{
+				var selected = selectedTags.FirstOrDefault(t => t.Id == tag.Id);
+
+				if (selected is not null)
+					tags.SelectedItems.Add(selected);
+			}
+
+			tags.SelectionChanged += OnSelectionChanged;
 		}
 	}
 }
